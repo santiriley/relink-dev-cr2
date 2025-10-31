3873572a5417e6198a7c7fbae877cb9946e47e24
santiriley <96429594+santiriley@users.noreply.github.com>
2025-10-31 14:25:29 -0600

    Add REPORT_SALT secret to config

--- functions/src/config.ts
+++ functions/src/config.ts
@@ -2,3 +2,4 @@ import { defineSecret } from 'firebase-functions/params';
 
 export const TELEMETRY_INGEST_TOKEN = defineSecret('TELEMETRY_INGEST_TOKEN');
 export const WHATSAPP_VERIFY_TOKEN  = defineSecret('WHATSAPP_VERIFY_TOKEN');
+export const REPORT_SALT            = defineSecret('REPORT_SALT');
